d176993fd1573a0905581dc0659e117b54580ca8
Rogier Spieker <rogier@konfirm.eu>
2021-11-17 11:11:31 +0100

    symbols may also be object keys

--- source/Guards/Object.ts
+++ source/Guards/Object.ts
@@ -1,19 +1,21 @@
 import { isObject } from "./Primitive";
 import { Guard, Validator, all, any, not } from "./Utility";
 
-export function isKey<T>(key: string): Guard<T> {
+type Key = string | symbol;
+
+export function isKey<T>(key: Key): Guard<T> {
 	return all(isObject, (value) => key in value);
 }
 
-export function isKeyOfType<T>(key: string, ...validators: Array<Validator>): Guard<T> {
+export function isKeyOfType<T>(key: Key, ...validators: Array<Validator>): Guard<T> {
 	return all<T>(isKey(key), (value: any) => validators.every((check) => check(value[key])));
 }
 
-export function isOptionalKeyOfType<T>(key: string, ...validators: Array<Validator>): Guard<T> {
+export function isOptionalKeyOfType<T>(key: Key, ...validators: Array<Validator>): Guard<T> {
 	return any<T>(all(isObject, not(isKey(key))), isKeyOfType(key, ...validators));
 }
 
-export function isStructure<T>(struct: { [key: string | symbol]: Validator }, optional: Array<keyof typeof struct> = []): Guard<T> {
+export function isStructure<T>(struct: { [key: Key]: Validator }, optional: Array<keyof typeof struct> = []): Guard<T> {
 	const validators: Array<Validator> = Object.keys(struct)
 		.map((key) => (optional.includes(key) ? isOptionalKeyOfType : isKeyOfType)<T>(key, struct[key]));
 
