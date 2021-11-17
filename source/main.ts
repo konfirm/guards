f7f5e966e52690c1e0defcda6f6a789e65463c29
Rogier Spieker <rogier@konfirm.eu>
2021-11-17 11:11:42 +0100

    export everything

---
+++ source/main.ts
@@ -0,0 +1,6 @@
+export * from './Guards/Array';
+export * from './Guards/Number';
+export * from './Guards/Object';
+export * from './Guards/Primitive';
+export * from './Guards/String';
+export * from './Guards/Utility';
